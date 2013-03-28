e6e9ab8c47765e516b4697160100009ffe2146bb
kenixsis <kenixsis@gmail.com>
2013-03-28 18:04:52 -0500

    Initial commit for HTC Ruby (Amaze 4G)
    Change-Id: Idd6953978366df62bdaad4a8ba879adbc3847f90

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -43,3 +43,6 @@ endif
 ifeq (pa_endeavoru,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_endeavoru.mk
 endif
+ifeq (pa_ruby,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_ruby.mk
+endif
---
+++ products/pa_ruby.mk
@@ -0,0 +1,59 @@
+# Copyright (C) 2012 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+#implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_ruby,$(TARGET_PRODUCT))
+
+# Define PA bootanimation size
+PARANOID_BOOTANIMATION_NAME := HDPI
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_hdpi
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= false
+
+# Include ParanoidAndroid common configuration
+include vendor/pa/config/pa_common.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, 
+device/htc/ruby/full_ruby.mk)
+
+# Product Package Extras - Repos can be added manually or via addprojects.py
+-include vendor/pa/packages/ruby.mk
+
+# CM Package Extras
+-include vendor/pa/packages/cm.mk
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_ruby
+PRODUCT_BRAND := HTC
+PRODUCT_MODEL := Amaze 4G
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ruby 
+TARGET_DEVICE=ruby 
+BUILD_FINGERPRINT="kenixsis" PRIVATE_BUILD_DESC="experimental"
+
+# Update local_manifest.xml
+GET_VENDOR_PROPS := $(shell vendor/pa/tools/getvendorprops.py 
+$(PRODUCT_NAME))
+GET_PROJECT_RMS := $(shell vendor/pa/tools/removeprojects.py 
+$(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pa/tools/addprojects.py 
+$(PRODUCT_NAME))
+GET_CM_PROJECT_ADDS := $(shell vendor/pa/tools/addprojects.py cm.adds)
+
+endif
